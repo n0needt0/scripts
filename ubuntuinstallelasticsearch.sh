6c333e73a7b2725fee71ec1904ce70a2de2b6d77
n0needt0 <andrewy@lasdorf.com>
2014-07-18 10:13:53 -0700

    Create ubuntuinstallelasticsearch.sh

---
+++ ubuntuinstallelasticsearch.sh
@@ -0,0 +1,25 @@
+apt-get update
+apt-get upgrade
+apt-get install curl git -y
+apt-get install python-software-properties -y
+apt-get install openjdk-7-jre-headless -y
+apt-get update
+
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.1.deb
+sudo dpkg -i elasticsearch-1.2.1.deb
+rm elasticsearch-1.2.1.deb
+
+sed -i "s/#cluster.name: elasticsearch/cluster.name: helpsearch/g" /etc/elasticsearch/elasticsearch.yml 
+sed -i "s/#node.name: \"Franz Kafka\"/node.name: \"$HOSTNAME\"/g" /etc/elasticsearch/elasticsearch.yml 
+sed -i 's/#transport.tcp.compress: true/transport.tcp.compress: true/g' /etc/elasticsearch/elasticsearch.yml 
+sed -i 's/#index.number_of_replicas: 1/index.number_of_replicas: 3/g' /etc/elasticsearch/elasticsearch.yml 
+sed -i 's/#discovery.zen.minimum_master_nodes: 1/discovery.zen.minimum_master_nodes: 2/g' /etc/elasticsearch/elasticsearch.yml
+
+/etc/init.d/elasticsearch restart
+
+/usr/share/elasticsearch/bin/plugin -i elasticsearch/marvel/latest
+/usr/share/elasticsearch/bin/plugin -install mobz/elasticsearch-head
+
+echo "marvel.agent.exporter.es.hosts: [\"search1:9200\",\"search3:9200\",\"search3:9200\"]" >> /etc/elasticsearch/elasticsearch.yml
+
+apt-get install ganglia-monitor nagios-nrpe-server -y
